16f8cdd14203cfa9f78e42af85448bceb93c72b3
AvikNayak <nayakavik19@gmail.com>
2025-01-27 13:44:36 +0530

    made the about section responsive

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -18,13 +18,13 @@ export default function About() {
         </h2>
         <div className="flex flex-col md:flex-row items-center gap-16">
           <div className="md:w-1/3">
-            <div className="relative group w-[400px] h-[400px]">
+            <div className="relative group w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]">
               <Image
                 src="/profile-photo.jpg"
                 alt="profile-photo"
-                width={400}
-                height={400}
-                className="rounded-full w-[400px] h-[400px] mx-auto border-4 border-emerald-400 shadow-2xl relative z-10 object-cover transition-transform duration-300 group-hover:scale-105"
+                width={500}
+                height={500}
+                className="rounded-full w-full h-full mx-auto border-4 border-emerald-400 shadow-2xl relative z-10 object-cover transition-transform duration-300 group-hover:scale-105"
               />
             </div>
           </div>
